9264b3c7f4567f606e9f50fd05d7d7f661a92d3a
Gaurav Sharma <gsharma.nus@gmail.com>
2023-06-01 17:51:09 +0800

    ✨Add simple emoji search with semantic ui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,41 @@
+import { useState } from 'react';
+import { EmojiRow } from './components/EmojiRow';
+import styled from 'styled-components/macro';
+import emojis from './resources/emojis.json';
+import { Input } from 'semantic-ui-react';
+
 export const App = () => {
-  return <div>Hi there</div>;
+  const [searchText, setSearchText] = useState('');
+
+  const filteredEmojis = emojis.filter((emoji) =>
+    emoji.name.includes(searchText),
+  );
+
+  return (
+    <AppWrapper>
+      <Input
+        fluid
+        icon="search"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearchText(e.target.value);
+        }}
+      />
+      <EmojiTableWrapper>
+        {filteredEmojis.map((emoji) => {
+          return <EmojiRow key={emoji.code} emoji={emoji} />;
+        })}
+      </EmojiTableWrapper>
+    </AppWrapper>
+  );
 };
+
+const AppWrapper = styled.div`
+  margin: 100px 25%;
+`;
+
+const EmojiTableWrapper = styled.div`
+  border: 1px solid #e7e7e7;
+  padding: 20px;
+  border-radius: 10px;
+`;
---
+++ src/components/EmojiRow.js
@@ -0,0 +1,37 @@
+import styled, {css} from 'styled-components/macro';
+
+export const EmojiRow = ({emoji}) => {
+    const {character, name, code} = emoji;
+
+
+    return (
+        <EmojiRowContainer>
+            <EmojiBoxAndName>
+                <EmojiBox>{character}</EmojiBox>
+                <span>{name}</span>
+            </EmojiBoxAndName>
+            <span>{code}</span>
+        </EmojiRowContainer>
+    );
+};
+
+const centreAlignedBox = css`
+  display: flex;
+  align-items: center;
+`;
+
+const EmojiRowContainer = styled.div`
+  ${centreAlignedBox}
+  justify-content: space-between;
+  border-bottom: 1px solid #e7e7e7;
+  padding: 5px;
+`;
+
+const EmojiBoxAndName = styled.div`
+  ${centreAlignedBox}
+`;
+
+const EmojiBox = styled.div`
+  margin-right: 20px;
+  font-size: 24px;
+`;
