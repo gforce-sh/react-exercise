0dc5a2f6e82ef905c97e23a703613b037a86819c
Gaurav Sharma <gsharma.nus@gmail.com>
2024-11-13 09:16:59 +0800

    Initial commit

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
-
-it('renders simple webpage with greeting', () => {
-  render(<App />);
-  expect(screen.getByText(/Hi there/i)).toBeInTheDocument();
-});
